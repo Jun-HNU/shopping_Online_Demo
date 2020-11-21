341b200bb4bae524809e34afa976c99cdc21b4fb
Jun-HNU <1248815798@qq.com>
2020-11-21 19:36:38 +0800

    add user-service model

---
+++ src/main/java/com/hnu/shopping/user/service/UserService.java
@@ -0,0 +1,12 @@
+package com.hnu.shopping.user.service;
+
+public interface UserService {
+    //List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId);
+
+    //List<UmsMember> getAllUser();
+/*    List<UmsMemberReceiveAddressmber> getAllUser();
+
+    List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId);
+
+    UmsMember login(UmsMember umsMember);*/
+}
