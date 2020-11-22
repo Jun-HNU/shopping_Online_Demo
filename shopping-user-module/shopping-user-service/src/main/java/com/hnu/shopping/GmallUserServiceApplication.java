b9a562cc4a739adbceabc7bb5a05794a33ec658f
Jun-HNU <1248815798@qq.com>
2020-11-22 20:45:04 +0800

    add shopping-user-web and shopping-user-service

---
+++ shopping-user-module/shopping-user-service/src/main/java/com/hnu/shopping/GmallUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.hnu.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.hnu.shopping.user.mapper")
+public class GmallUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.hnu.shopping.GmallUserServiceApplication.class, args);
+	}
+
+}
---
+++ shopping-user-module/shopping-user-service/src/main/java/com/hnu/shopping/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,7 @@
+package com.hnu.shopping.user.mapper;
+
+import com.hnu.shopping.bean.UmsMemberReceiveAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress>{
+}
